e2dd1307dfbe25c991abea7699c92b61c2127593
chenglin <837460387@qq.com>
2019-04-02 20:26:40 +0800

    cart logic code submit

--- server/routes/users.js
+++ server/routes/users.js
@@ -73,4 +73,123 @@ router.get('/checkLogin', (req, res, next) => {
     }
 })
 
+router.get('/cartList', (req, res, next) => {
+    let userId = req.cookies.userId
+    User.findOne({
+        userId: userId
+    }, (err, doc) => {
+        if (err) {
+            res.json({
+                status: '1',
+                msg: '',
+                result: err.message
+            })
+        } else {
+            if (doc) {
+                res.json({
+                    status: '0',
+                    msg: '',
+                    result: doc.cartList
+                })
+            }
+        }
+    })
+})
+
+router.post('/cartDel', (req, res, next) => {
+    let userId = req.cookies.userId
+    let productId = req.body.productId
+    User.update({
+        userId: userId
+    }, {
+        $pull: {
+            "cartList": {
+                'productId': productId
+            }
+        }
+    }, (err, doc) => {
+        if (err) {
+            res.json({
+                status: '1',
+                msg: '',
+                result: err.massge
+            })
+        } else {
+            res.json({
+                status: '0',
+                msg: '',
+                result: doc
+            })
+        }
+    })
+})
+
+router.post('/editCheckAll', (req, res, next) => {
+    let userId = req.cookies.userId
+    let checkAll = req.body.checkAll
+
+    User.findOne({
+        userId: userId
+    }, (err, user) => {
+        if (err) {
+            res.json({
+                status: '1',
+                msg: '',
+                result: err.massge
+            })
+        } else {
+            if (user) {
+                user.cartList.forEach((item) => {
+                    item.checked = checkAll
+                })
+                user.save((err1, doc) => {
+                    if (err1) {
+                        res.json({
+                            status: '1',
+                            msg: '',
+                            result: err.messge
+                        })
+                    }
+                })
+                res.json({
+                    status: '0',
+                    msg: '',
+                    result: 'success'
+                })
+            }
+        }
+    })
+})
+
+// 编辑购物车
+router.post('/editCart', (req, res, next) => {
+    let userId = req.cookies.userId
+    let productId = req.body.productId
+    let productNum = req.body.productNum
+    let checked = req.body.checked
+    User.update({
+        "userId": userId,
+        "cartList.productId": productId
+    }, {
+        "cartList.$.productNum": productNum,
+        "cartList.$.checked": checked
+    }, (err, doc) => {
+        if (err) {
+            res.json({
+                status: '1',
+                msg: '',
+                result: err.massge
+            })
+        } else {
+            if (doc) {
+                res.json({
+                    status: '0',
+                    msg: '',
+                    result: "success"
+                })
+            }
+        }
+    })
+})
+
 module.exports = router;
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -59,42 +59,46 @@
               </ul>
             </div>
             <ul class="cart-item-list">
-              <li>
+              <li v-for="(item, idx) in cartList" :key="idx">
                 <div class="cart-tab-1">
                   <div class="cart-item-check">
-                    <a href="javascipt:;" class="checkbox-btn item-check-btn">
+                    <a
+                      href="javascipt:;"
+                      class="checkbox-btn item-check-btn"
+                      :class="{'check':item.checked==1}"
+                      @click="editCart('checked',item)">
                       <svg class="icon icon-ok">
                         <use xlink:href="#icon-ok"></use>
                       </svg>
                     </a>
                   </div>
                   <div class="cart-item-pic">
-                    <img src="/static/1.jpg">
+                    <img :src="'/static/'+item.productImage" :alt="item.productName">
                   </div>
                   <div class="cart-item-title">
-                    <div class="item-name">XX</div>
+                    <div class="item-name">{{item.productName}}</div>
                   </div>
                 </div>
                 <div class="cart-tab-2">
-                  <div class="item-price">1000</div>
+                  <div class="item-price">{{item.salePrice}}</div>
                 </div>
                 <div class="cart-tab-3">
                   <div class="item-quantity">
                     <div class="select-self select-self-open">
                       <div class="select-self-area">
-                        <a class="input-sub">-</a>
-                        <span class="select-ipt">10</span>
-                        <a class="input-add">+</a>
+                        <a class="input-sub" @click="editCart('minu', item)">-</a>
+                        <span class="select-ipt">{{item.productNum}}</span>
+                        <a class="input-add" @click="editCart('add', item)">+</a>
                       </div>
                     </div>
                   </div>
                 </div>
                 <div class="cart-tab-4">
-                  <div class="item-price-total">100</div>
+                  <div class="item-price-total">{{item.salePrice*item.productNum}}</div>
                 </div>
                 <div class="cart-tab-5">
                   <div class="cart-item-opration">
-                    <a href="javascript:;" class="item-edit-btn">
+                    <a href="javascript:;" class="item-edit-btn" @click="delCartBtn(item.productId)">
                       <svg class="icon icon-del">
                         <use xlink:href="#icon-del"></use>
                       </svg>
@@ -109,8 +113,8 @@
           <div class="cart-foot-inner">
             <div class="cart-foot-l">
               <div class="item-all-check">
-                <a href="javascipt:;">
-                  <span class="checkbox-btn item-check-btn">
+                <a href="javascipt:;" @click="toggleCheckedAll">
+                  <span class="checkbox-btn item-check-btn" :class="{'check':checkedAllFlag}">
                       <svg class="icon icon-ok"><use xlink:href="#icon-ok"/></svg>
                   </span>
                   <span>Select all</span>
@@ -119,7 +123,7 @@
             </div>
             <div class="cart-foot-r">
               <div class="item-total">
-                Item total: <span class="total-price">500</span>
+                Item total: <span class="total-price">{{totalPrice}}</span>
               </div>
               <div class="btn-wrap">
                 <a class="btn btn--red">Checkout</a>
@@ -129,9 +133,132 @@
         </div>
       </div>
     </div>
+    <modal :mdShow="mdCartConfirm" @close="closeModal">
+      <p slot="message">你确定删除此条数据?</p>
+      <div slot="btnGroup">
+        <a class="btn btn--m" href="javascript:;" @click="cartDel">确定</a>
+        <a class="btn btn--m" href="javascript:;" @click="mdCartConfirm=false">关闭</a>
+      </div>
+    </modal>
     <nav-footer></nav-footer>
   </div>
 </template>
+
+<script>
+	import './../assets/css/checkout.css'
+  import NavHeader from '@/components/NavHeader.vue'
+  import NavFooter from '@/components/NavFooter.vue'
+  import NavBread from '@/components/NavBread.vue'
+  import Modal from '@/components/Modal'
+  import axios from 'axios'
+
+  export default {
+    data() {
+      return {
+        cartList: [],
+        mdCartConfirm: false,
+        productId: ''
+      }
+    },
+    components: {
+      NavHeader,
+      NavBread,
+      NavFooter,
+      Modal
+    },
+    mounted() {
+      this.init()
+    },
+    computed: {
+      checkedAllFlag() {
+        return this.checkedCount == this.cartList.length
+      },
+      checkedCount() {
+        let i = 0
+        this.cartList.forEach((item) => {
+          if (item.checked) {
+            i++
+          }
+        })
+        return i++
+      },
+      totalPrice() {
+        let money = 0
+        this.cartList.forEach((item) => {
+          if (item.checked) {
+            money += parseInt(item.salePrice) * parseInt(item.productNum)
+          }
+        })
+        return money
+      }
+    },
+    methods: {
+      init() {
+        axios.get('/users/cartList').then((response) => {
+          let res = response.data
+          if (res.result) {
+            this.cartList = res.result
+          }
+        })
+      },
+      closeModal() {
+        this.mdCartConfirm = false
+      },
+      cartDel() {
+        axios.post('/users/cartDel', {
+          productId: this.productId
+        }).then((response) => {
+          let res = response.data
+          if (res.status == '0') {
+            this.mdCartConfirm = false
+            this.init()
+          }
+        })
+      },
+      delCartBtn(productId) {
+        this.mdCartConfirm = true
+        this.productId = productId
+      },
+      toggleCheckedAll() {
+        let flag = !this.checkedAllFlag
+        this.cartList.forEach((item) => {
+          item.checked = flag ? 1 : 0
+        })
+        axios.post('/users/editCheckAll', {
+          checkAll: this.checkedAllFlag
+        }).then((response) => {
+          let res = response.data
+          if (res.status == '0') {
+            console.log('update');
+          }
+        })
+      },
+      editCart(flag, item) {
+        if (flag == "add") {
+          item.productNum++
+        } else if (flag == "minu") {
+          if (item.productNum <= 1) {
+            return;
+          }
+          item.productNum--
+        } else {
+          item.checked = item.checked == "1" ? '0' : '1';
+        }
+        axios.post('/users/editCart', {
+          productNum: item.productNum,
+          productId: item.productId,
+          checked: item.checked
+        }).then((response) => {
+          let res = response.data
+          if (res.status == '0') {
+            console.log("edit cart success");
+          }
+        })
+      }
+    }
+  }
+</script>
+
 <style>
   .input-sub,.input-add{
     min-width: 40px;
@@ -156,17 +283,3 @@
     text-align: center;
   }
 </style>
-<script>
-  import NavHeader from '@/components/NavHeader.vue'
-  import NavFooter from '@/components/NavFooter.vue'
-	import NavBread from '@/components/NavBread.vue'
-	import './../assets/css/checkout.css'
-
-  export default {
-    components: {
-      NavHeader,
-      NavBread,
-      NavFooter
-    }
-  }
-</script>
